5e49e0516d300f3e0113aeec9dce426b53f6c79a
Opilane TTHK <opilane@tthk.ee>
2023-09-05 15:53:54 +0300

    init3

--- Shop/Shop/Models/SpaceshipsCreateViewModel.cs
+++ Shop/Shop/Models/SpaceshipsCreateViewModel.cs
@@ -2,5 +2,13 @@
 {
     public class SpaceshipsCreateViewModel
     {
+        public Guid? ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Passengers { get; set; }
+        public int EnginePower {get; set;}
+        public string FuelType { get; set;}
+        public int FuelCapacity { get; set;}
+        public string Company { get; set;}
     }
 }
