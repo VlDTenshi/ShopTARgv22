5e92487a32f9e673875606da1652c66cfd5d24f4
Opilane TTHK <opilane@tthk.ee>
2023-09-11 12:06:14 +0300

    init8

--- Shop/Shop/Controllers/SpaceshipsController.cs
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models.Spaceship;
 
 namespace Shop.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopContext _context;
+
+        public SpaceshipsController(ShopContext context) 
+        
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    ID=x.Id,
+                    Name=x.Name,
+                    Type=x.Type,
+                    EnginePower=x.EnginePower,
+                    Passengers=x.Passengers,
+                });
+            return View(result);
         }
 
         [HttpGet]
---
+++ Shop/Shop/Models/Spaceship/SpaceshipsIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Models.Spaceship
+{
+    public class SpaceshipIndexViewModel
+    {
+        public Guid? ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Passengers { get; set; }
+        public int EnginePower { get; set; }
+        
+    }
+}
