19667f40decf3639acc1cada39f35f7ad8b37c35
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-10-30 14:36:37 +0200

    Kindergarden xUnit
    Second step - creating MockHostEnvironment

--- Shop/Shop.Kindergarden.Test/Mock/MockHostEnvironment.cs
+++ Shop/Shop.Kindergarden.Test/Mock/MockHostEnvironment.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace Shop.Kindergarden.Test.Mock
 {
     public class MockHostEnvironment: IHostEnvironment
     {
+        public string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
