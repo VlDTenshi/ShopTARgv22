9ec3ef19272337c17f38c4baacc9382e27d2011d
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-10-30 15:37:43 +0200

    Kindergarden xUnit
    Unittest3 class creating

--- Shop/Shop.Kindergarden.Test/UnitTest3.cs
+++ Shop/Shop.Kindergarden.Test/UnitTest3.cs
@@ -1,11 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shop.ApplicationServices.Services;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
+using Shop.Kindergarden.Test.Macros;
+using Shop.Kindergarden.Test.Mock;
+
 namespace Shop.Kindergarden.Test
 {
-    public class UnitTest3
+    public abstract class UnitTest3
     {
-        [Fact]
-        public void Test1()
+        protected IServiceProvider serviceProvider {  get; }
+
+        protected UnitTest3 ()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        public void Dispose()
         {
 
+        }
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+        protected T Macro<T>() where T : IMacros
+        {
+            return serviceProvider.GetService<T>();
+        }
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IKinderGardenServices, KinderGardenServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockHostEnvironment>();
+
+            services.AddDbContext<ShopContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+            RegisterMacros(services);
+        }
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macrosBaseType = typeof(IMacros);
+
+            var macros = macrosBaseType.Assembly.GetTypes()
+                .Where(x => macrosBaseType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
         }
     }
 }
