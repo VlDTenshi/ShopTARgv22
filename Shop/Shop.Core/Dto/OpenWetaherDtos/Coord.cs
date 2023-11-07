2cbcfaee03b43b4d763b3dbd758234a2f05b274c
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-07 11:14:17 +0200

    coord

---
+++ Shop/Shop.Core/Dto/OpenWetaherDtos/Coord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Dto.OpenWetaherDtos
+{
+	public class Coord
+	{
+		[JsonPropertyName("lon")]
+		public double Lon {  get; set; }
+		[JsonPropertyName("lat")]
+		public double Lat { get; set; }
+	}
+}
