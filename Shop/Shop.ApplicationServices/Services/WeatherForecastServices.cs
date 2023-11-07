d2fd786baa45efccdfbb408c6547617cf8007593
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-07 11:14:03 +0200

    service&dto

--- Shop/Shop.ApplicationServices/Services/WeatherForecastServices.cs
+++ Shop/Shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using Shop.Core.Dto.OpenWetaherDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shop.ApplicationServices.Services
 {
-	public class WeatherForecastServices
+    public class WeatherForecastServices
 	{
 		public async Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto)
 		{
 			string idOpenWeather = "7a1e82109b389955967ad299cc91f8ff";
 			string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={idOpenWeather}";
+
+			using (WebClient client = new WebClient())
+			{
+
+			}
+			return dto;
 		}
 	}
 }
