e65eb8f4f131410998546e228725dd71f95ddbf2
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-07 11:15:11 +0200

    wind

---
+++ Shop/Shop.Core/Dto/OpenWetaherDtos/Wind.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shop.Core.Dto.OpenWetaherDtos
+{
+	public class Wind
+	{
+		[JsonPropertyName("speed")]
+		public double wind_Speed { get; set; }
+		[JsonPropertyName("deg")]
+		public int wind_Deg { get; set; }
+		[JsonPropertyName("gust")]
+		public double wind_Gust { get; set; }
+	}
+}
