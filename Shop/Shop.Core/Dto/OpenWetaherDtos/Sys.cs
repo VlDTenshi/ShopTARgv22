1532571eacf512b05616dd87551ccd7b415406bd
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-07 11:15:01 +0200

    sys

---
+++ Shop/Shop.Core/Dto/OpenWetaherDtos/Sys.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shop.Core.Dto.OpenWetaherDtos
+{
+	public class Sys
+	{
+		[JsonPropertyName("type")]
+		public int sys_Type { get; set; }
+		[JsonPropertyName("id")]
+		public int sys_Id { get; set; }
+		[JsonPropertyName("country")]
+		public string sys_Country { get; set; }
+		[JsonPropertyName("sunrise")]
+		public int sys_sunrise { get; set; }
+		[JsonPropertyName("sunset")]
+		public int sys_sunset { get; set; }
+	}
+}
