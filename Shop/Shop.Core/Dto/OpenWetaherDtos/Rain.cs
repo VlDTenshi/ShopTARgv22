ab464f11304e03c103f8e12f7fe42c7656f04690
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-07 11:14:56 +0200

    rain

---
+++ Shop/Shop.Core/Dto/OpenWetaherDtos/Rain.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shop.Core.Dto.OpenWetaherDtos
+{
+	public class Rain
+	{
+		[JsonPropertyName("1h")]
+		public double rain_1h { get; set; }
+	}
+}
