1f4437976f00864f560a78e1908ab32dad33579a
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-12-12 13:19:43 +0200

    Fixes

---
+++ Shop/Shop/Controllers/ChatHubController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Shop.Controllers
+{
+	public class ChatHubController : Controller
+	{
+		private readonly ILogger<ChatHubController> _logger;
+
+		public ChatHubController(ILogger<ChatHubController> logger)
+		{
+			_logger = logger;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -5,6 +5,7 @@ using Shop.ApplicationServices.Services;
 using Shop.Core.Domain;
 using Shop.Core.ServiceInterface;
 using Shop.Data;
+using Shop.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +29,9 @@ builder.Services.AddScoped<IChuckNorrisServices, ChuckNorrisServices>();
 builder.Services.AddScoped<ICoctailServices, CoctailServices>();
 builder.Services.AddScoped<IAccuWeatherServices, AccuWeatherServices>();
 builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddSignalR();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
@@ -61,7 +65,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapHub<ChatHub>("/ChatHub/chatHub");
 app.Run();
 
 
